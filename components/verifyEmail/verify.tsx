c087e0b59c9941b522c289e3e0d6e56d4a5ab136
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-03-09 09:58:59 +0600

    completed the verify email main logic part

--- components/verifyEmail/verify.tsx
+++ components/verifyEmail/verify.tsx
@@ -2,18 +2,25 @@
 
 import React, {FormEvent, useState} from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 const  VerifyEmail = () => {
   const [otpCode, setOtpCode] = useState("");
+  const router = useRouter();
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setOtpCode(e.target.value);
   }
-  const handleSubmit = (e:FormEvent<HTMLFormElement>) => { 
+  const handleSubmit = async(e:FormEvent<HTMLFormElement>) => { 
     e.preventDefault();
-    console.log("I am in handle submit");
+    const otp = {'otpCode': otpCode}
+    const req = await axios.post("http://127.0.0.1:8000/api/verify-email", otp)
+    if (req.status === 200) {
+      router.push('/signin')
+    }
   };
   return (
     <div className='flex flex-col items-center mt-60 gap-2'>
+      <h1>Please Enter the otp code that has been sent to your  email address</h1>
       <form onSubmit={handleSubmit} className='flex flex-col items-center gap-3'>
         <label htmlFor="otp">Enter your Otp code:</label>
         <input type="text" name='otp' id='otp' value={otpCode} onChange={handleChange} className='text-red-900 ' />
